35a24d262a1807e18b262672fe7421c3d00f4c54
MerlinPeter <merlinahila@gmail.com>
2017-03-02 18:30:42 -0800

    merl changes

--- Hunter_App/GameScene.swift
+++ Hunter_App/GameScene.swift
@@ -9,7 +9,47 @@
 import SpriteKit
 import GameplayKit
 
+
 class GameScene: SKScene {
+    
+  //  let  manager  = CMMotionManager()
+    
+    var Player = SKSpriteNode(imageNamed: "Fox.png" )
+    
+    var player: SKSpriteNode!
+
+    
+    
+    override func sceneDidLoad() {
+        Player.position = CGPoint(x:self.frame.size.width / 2, y:self.frame.size.height / 2)
+        
+        self.addChild(Player)
+
+        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        // 2
+        borderBody.friction = 1.5
+        // 3
+        self.physicsBody = borderBody
+
+        
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x:50,y:100))
+        let followLine = SKAction.follow(path, asOffset: true, orientToPath: false, duration: 3.0)
+        let reversedLine = followLine.reversed()
+        let square = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let followSquare = SKAction.follow(square.cgPath, asOffset: true, orientToPath: false, duration:5.0)
+        
+        //Player.run(SKAction.sequence([followLine, reversedLine,followSquare]))
+        
+        
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
@@ -43,5 +83,16 @@ class GameScene: SKScene {
     }
     
     
-    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "fox.png")
+        player.position = CGPoint(x: 96, y: 672)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody!.allowsRotation = false
+        player.physicsBody!.linearDamping = 0.5
+        
+        player.physicsBody!.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody!.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody!.collisionBitMask = CollisionTypes.wall.rawValue
+        addChild(player)
+    }
     }
