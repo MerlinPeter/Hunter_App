82056108a14889c19ca571f68fe70e104d0bb952
MerlinPeter <merlinahila@gmail.com>
2017-03-18 23:30:20 -0700

    leaderboard/achievement integration

--- Hunter_App/GameViewController.swift
+++ Hunter_App/GameViewController.swift
@@ -32,7 +32,7 @@ class GameViewController: UIViewController {
         
        if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameStartScene") {
+            if let scene = SKScene(fileNamed: "GameWin") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
